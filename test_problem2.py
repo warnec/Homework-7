9f9eafd4e321f4246a6cf26475699c8e6071deec
warnec <warnec@oregonstate.edu>
2021-03-05 23:54:00 -0800

    wrote first test to check for divisibility by 4

---
+++ test_problem2.py
@@ -0,0 +1,11 @@
+import unittest
+from problem2 import testLeap
+
+
+class TestCase(unittest.TestCase):
+
+    def test_leap(self):
+        self.assertEqual(testLeap(4),"The entered year is a leap year")
+        
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
