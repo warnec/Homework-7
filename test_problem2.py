5dc19114b98c83f483c8e916eba4470d29168d97
warnec <warnec@oregonstate.edu>
2021-03-06 00:09:53 -0800

    wrote test for checking for non integer inputs

--- test_problem2.py
+++ test_problem2.py
@@ -10,5 +10,8 @@ class TestCase(unittest.TestCase):
         self.assertEqual(testLeap(400),"The entered year is a leap year")
     def test_leap2(self):
         self.assertEqual(testLeap(2000),"The entered year is a leap year")
+    def test_leap3(self):
+        self.assertEqual(testLeap("f"), None)
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
