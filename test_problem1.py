195c85404a83d8ec11b0dbfc316f301a390d9d10
warnec <warnec@oregonstate.edu>
2021-03-05 23:36:44 -0800

    made test for problem 1

---
+++ test_problem1.py
@@ -0,0 +1,11 @@
+import unittest
+from problem1 import FizzBuzz
+
+
+class TestCase(unittest.TestCase):
+
+    def test_fizz(self):
+        self.assertEqual(FizzBizz(),"12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FizzBuzz3132Fizz34BuzzFizz3738FizzBuzz41Fizz4344FizzBuzz4647Fizz49BuzzFizz5253FizzBuzz56Fizz5859FizzBuzz6162Fizz64BuzzFizz6768FizzBuzz71Fizz7374FizzBuzz7677Fizz79BuzzFizz8283FizzBuzz86Fizz8889FizzBuzz9192Fizz94BuzzFizz9798Fizz")
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
