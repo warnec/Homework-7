b273ea812bc1ec19676debfe6b2535e9a0530395
warnec <warnec@oregonstate.edu>
2021-03-06 00:02:03 -0800

    wrote test for testing divisibility of 4 and 100

--- test_problem2.py
+++ test_problem2.py
@@ -6,6 +6,8 @@ class TestCase(unittest.TestCase):
 
     def test_leap(self):
         self.assertEqual(testLeap(4),"The entered year is a leap year")
-        
+    def test_leap(self):
+        self.assertEqual(testLeap(400),"The entered year is a leap year")
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
