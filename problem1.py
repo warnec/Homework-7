c36482c619e767123d31afe8e714cd863595b41b
warnec <warnec@oregonstate.edu>
2021-03-05 23:42:57 -0800

    problem 1 code completed

--- problem1.py
+++ problem1.py
@@ -1,5 +1,17 @@
 def FizzBizz():
-    pass
+    string = ""
+    for i in range(1, 100):
+
+        if i % 3 == 0:
+            string = string + "Fizz"
+
+        if i % 5 == 0:
+            string = string + "Buzz"
+        if (i % 5 != 0) and (i % 3 != 0):
+            string = string + str(i)
+    print(string)
+    return string
 
 
 
+FizzBizz()
